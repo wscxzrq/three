a0c89cee7754043871a55f27395e3db3a4ad2f01
t-jie.xu3 <18330156357@163.com>
2024-05-30 21:44:21 +0800

    坐标辅助器与辅助控制器

---
+++ 01.第一个 threejs 应用/01-start-app/src/main01-控制器和辅助坐标系.js
@@ -0,0 +1,66 @@
+
+import * as three from 'three'
+// 导入轨道控制器
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// 创建场景
+const scene = new three.Scene();
+
+// 创建相机
+const camera = new three.PerspectiveCamera(
+  45, // 视角 
+  window.innerWidth / window.innerHeight, // 宽高比
+  0.1, // 近平面
+  1000 // 远平面
+);
+
+// 创建渲染器
+const renderer = new three.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement)
+
+// 创建几何体
+const geometry = new three.BoxGeometry(1, 1, 1);
+
+// 创建材质
+const material = new three.MeshBasicMaterial({ color: 0x00ff00 });
+
+// 创建网格
+const cube = new three.Mesh(geometry, material);
+
+// 将网格添加到场景
+scene.add(cube);
+
+// 设置相机位置
+camera.position.z = 5;
+camera.position.y = 2;
+camera.position.x = 2;
+camera.lookAt(0, 0, 0);
+
+// 添加世界坐标辅助器
+const axesHelper = new three.AxesHelper(5);
+scene.add(axesHelper)
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, document.body);
+// 设置带阻尼的惯性
+controls.enableDamping = true;
+// 设置阻尼的系数
+controls.dampingFactor = 0.05;
+// 设置自动旋转
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+// 渲染
+function animate(){
+  requestAnimationFrame(animate);
+
+  // 旋转
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
+  
+  // 更新轨道控制器状态
+  controls.update()
+  renderer.render(scene, camera);
+}
+
+animate()
---
+++ 02-结合 vue 进行开发/threeJsVue/src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import * as three from 'three'
+// 创建场景
+const scene = new three.Scene();
+
+// 创建相机
+const camera = new three.PerspectiveCamera(
+  45, // 视角 
+  window.innerWidth / window.innerHeight, // 宽高比
+  0.1, // 近平面
+  1000 // 远平面
+);
+
+// 创建渲染器
+const renderer = new three.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement)
+
+// 创建几何体
+const geometry = new three.BoxGeometry(1, 1, 1);
+
+// 创建材质
+const material = new three.MeshBasicMaterial({ color: 0x00ff00 });
+
+// 创建网格
+const cube = new three.Mesh(geometry, material);
+
+// 将网格添加到场景
+scene.add(cube);
+
+// 设置相机位置
+camera.position.z = 10;
+
+// 渲染
+function animate(){
+  requestAnimationFrame(animate);
+
+  // 旋转
+  cube.rotation.x += 0.02;
+  cube.rotation.y += 0.02;
+
+  renderer.render(scene, camera);
+}
+
+animate()
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
